7573fe2ad0108758aec5a97b199cb988ba218643
TimiH <hestim@gmail.com>
2021-03-03 13:17:52 +0100

    Teamgame is no finished, rearranged some functions and added Comments

--- PlayerModels/staticBidding.py
+++ PlayerModels/staticBidding.py
@@ -1,28 +1,45 @@
 from helper import createTrumps
 from CardValues import RANKS, SUITS
 
+#Returns best TeamGame or (None,None) if no gamemode possible or sensible
 def choseTeamGame(validBids, hand):
     possibleTeam = list(filter(lambda x: x[0] == 1, validBids))
     ret = (None, None)
     if not possibleTeam:
         return (None, None)
 
+    #Counting U,O,T,A and
     uCount = countCardInHand(hand,'U')
     oCount = countCardInHand(hand,'O')
-    tCount = countColoursInHand(hand,'Herz')
+    tCount = int(len(trumpsInHandByGamemode(hand,(1,))))
     aCount = countCardInHand(hand,'A')
-    colourCount = countColoursInHand(hand)
+    colourCount = countColoursInHand(hand) #Counts[E,G,H,S]
     total = uCount + oCount + tCount
+    print('Total: '+str(total),'Ober: ' +str(oCount),' Unter:' + str(uCount), ' #Asse:' + str(aCount))
+
     # Decide if possible otherwise delete list
-    if total >= 4 and oCount >= 1 and uCount >= 1 and aCount >= 2 and any(
-            x.rank == 'O' and x.rank in ['Eichel', 'Gras', 'Herz'] for x in hand):
+    #6 Trump or better
+    if total >= 6:
+        print('Option 6 Trump')
         pass
-    elif total >= 5 and oCount >= 2 and uCount >= 1 and all(x > 0 for x in colourCount):
-        pass
-    elif total >= 5 and (colourCount[0] == 0 or colourCount[1] == 0 or colourCount[3] == 0):
-        pass
-    elif total >= 6:
-        pass
+    #5 Trump
+    elif total == 5:
+        #1 colour missing/Ace
+        if any(x.rank == 'A' and x.suit != 'Herz' for x in hand) or any(colourCount[x] == 0 for x in [0,1,3]):
+            print('Option 5 Trump: Fehl or Ace')
+            pass
+        #2O & U
+        elif oCount >= 2 and uCount <= 1:
+            print('Option 5 Trump: 2O&1U')
+            pass
+    #4 Trump containing 1 out of [OE,OG,OH], 1 U, and two Aces not heart
+    elif total ==4:
+        if oCount == 1 and uCount == 1 and any(x.rank == 'O' and x.suit in ['Eichel', 'Gras', 'Herz'] for x in hand):
+            #two non trump aces
+            acesInHand = list(filter(lambda x: x.rank == 'A' and x.suit != 'Herz', hand))
+            if len(acesInHand) >= 2:
+                print('Option 4 Trump')
+                pass
     else:
         possibleTeam = []
 
@@ -62,7 +79,6 @@ def choseSoloGame(validBids, hand):
 
     return chosenSolo
 
-
 # Possibly look at 2U(top 2),2A
 def choseWenzGame(validBids, hand):
     uCount = countCardInHand(hand, 'U')
@@ -79,12 +95,16 @@ def choseWenzGame(validBids, hand):
     return ret
 
 
+def choseWenzGame2(validBids, hand):
+    pass
+
+# Returns a set of Trumps in hand
 def trumpsInHandByGamemode(hand, gameMode):
     trumps = createTrumps(gameMode)
     trumpsInHand = set(hand) & trumps
     return trumpsInHand
 
-
+# Returns count of Rank
 def countCardInHand(hand, rank):
     count = 0
     for card in hand:
@@ -92,25 +112,27 @@ def countCardInHand(hand, rank):
             count += 1
     return count
 
-
-# Returns either a count for all colours as list [E,G,H,S] or count for specific colour
-def countColoursInHand(hand, colour=None):
+# Returns either a count of A,T,9,8,7 for all colours as list [E,G,H,S]
+def countColoursInHand(hand):
     counts = [0, 0, 0, 0]
-    if colour == None:
-        for card in hand:
-            if card.rank not in ['U', 'O']:
-                counts[SUITS[card.suit]] += 1
-        return counts
-    else:
-        count = 0
-        for card in hand:
-            if card.suit == colour and card.rank not in ['U', 'O']:
-                count += 1
-        return count
+    for card in hand:
+        if card.rank not in ['U', 'O']:
+            counts[SUITS[card.suit]] += 1
+    return counts
 
+
+# Returns count of A,T,9,8,7 for all colour
+def countColourOfSuit(hand,colour):
+    count = 0
+    for card in hand:
+        if card.suit == colour and card.rank not in ['U', 'O']:
+            count += 1
+    return count
+
+#Count T,9,8,7 of suir
 def countSpatzenOfSuit(hand, suit):
     count = 0
     for card in hand:
-        if card.rank != 'A' and card.suit != suit:
+        if card.rank not in ['O','U','A'] and card.suit != suit:
             count += 1
     return count
