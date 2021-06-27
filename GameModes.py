51c04ab314f07565d8eed04e248b59d9bcde1cc5
TimiH <hestim@gmail.com>
2021-06-28 00:35:40 +0200

    none to 0

--- GameModes.py
+++ GameModes.py
@@ -7,5 +7,5 @@ SOLO = 3
 #NoGame,3 Team games(Hearts Not allowed), Wenz, 4 Solo
 MODES = [(NONE, NONE),
          (TEAM, SUITS['Eichel']), (TEAM, SUITS['Gras']), (TEAM, SUITS['Schellen']),
-         (2, None),
+         (2, 0),
          (3, SUITS['Eichel']), (3, SUITS['Gras']), (3, SUITS['Herz']), (3, SUITS['Schellen'])]
