b4e74e9490c126033638ac2a641140647a0f98ea
TimiH <hestim@gmail.com>
2021-06-30 17:50:35 +0200

    big rewrite

--- Trick.py
+++ Trick.py
@@ -1,8 +1,8 @@
 from Card import Card
 from CardValues import RANKS, VALUES, SUITS, REVERSEDSUITS
-from helper import createTrumps, createTrumpsList
-from PlayerModels.staticBidding import getCardsOfSuit, cardInHand
-from copy import deepcopy, copy
+from helper import createTrumpsList
+from PlayerModels.staticBidding import getCardsOfSuit, cardInHand, trumpsInHandByGamemode
+from copy import deepcopy
 
 
 class Trick:
@@ -23,22 +23,118 @@ class Trick:
         if self.gameDict['gameMode'][0] == 1:
             suit = self.gameDict['gameMode'][1]
             searchedSuit = REVERSEDSUITS[suit]
+            startCard = self.history[0]
             if len(self.history) == 4 and not self.gameDict['ranAway'] and not self.gameDict['searched']:
-                if self.history[0].suit == searchedSuit and not cardInHand(self.history, searchedSuit, 'A'):
+                if startCard not in self.gameDict['trumpcards'] and self.history[
+                    0].suit == searchedSuit and not cardInHand(self.history, searchedSuit, 'A'):
                     self.gameDict['ranAway'] = True
             if card.rank == 'A' and card.suit == reversed[suit]:
                 self.gameDict['searched'] = True
 
+    def getValidActionsForHand(self, playerHand):
+        if len(playerHand) == 1:
+            return playerHand
+        if self.gameMode[0] > 1:
+            validActions = self.getValidActionsWenzSolo(playerHand)
+            return validActions
+        else:
+            # searched Player?
+            suitSearched = REVERSEDSUITS[self.gameMode[1]]
+            ace = Card(suitSearched, 'A')
+            trumps = self.gameDict['trumpCards']
+            # Partner
+            if ace in playerHand:
+                # Lead
+                if len(self.history) == 0:
+                    # once Searche/runawayPosssable player can play whatever
+                    if self.gameDict['runAwayPossible'] or self.gameDict['ranAway']:
+                        return playerHand
+                    # while not searched cannot search yourself
+                    if not self.gameDict['ranAway']:
+                        searchCardsNotA = set(getCardsOfSuit(playerHand, suitSearched, ['O', 'U', 'A']))
+                        validCards = set(playerHand) - searchCardsNotA
+                        return list(validCards)
+                # No Lead
+                else:
+                    card0 = self.history[0]
+                    # play if searched
+                    if card0 not in trumps and card0.suit == suitSearched:
+                        return [ace]
+                    validcards = []
+                    # trump played
+                    if card0 in trumps:
+                        trumpInHand = trumpsInHandByGamemode(playerHand, self.gameMode)
+                        if trumpInHand:
+                            return trumpInHand
+                        else:
+                            validcards = playerHand
+                    # no trump played
+                    else:
+                        cardsOfSuit = getCardsOfSuit(playerHand, card0.suit, ['U', 'O'])
+                        if cardsOfSuit:
+                            return cardsOfSuit
+                        else:
+                            validcards = playerHand
+
+                    # test if player can play Ace
+                    if ace in validcards:
+                        if not self.gameDict['ranAway'] or not self.gameDict['searched']:
+                            validcards.remove(ace)
+                    return validcards
+
+            # Other Players
+            else:
+                # Lead
+                if len(self.history) == 0:
+                    return playerHand
+                else:
+                    card0 = self.history[0]
+                    if card0 in trumps:
+                        trumpInHand = trumpsInHandByGamemode(playerHand, self.gameMode)
+                        if trumpInHand:
+                            return trumpInHand
+                        else:
+                            return playerHand
+                    else:
+                        cardsOfSuit = getCardsOfSuit(playerHand, card0.suit, ['U', 'O'])
+                        if cardsOfSuit:
+                            return cardsOfSuit
+                        else:
+                            return playerHand
+
+    def getValidActionsWenzSolo(self, playerHand):
+        if len(self.history) == 0:
+            return playerHand
+        else:
+            card0 = self.history[0]
+            trumps = self.gameDict['trumpCards']
+            if card0 in trumps:
+                trumpInHand = trumpsInHandByGamemode(playerHand, self.gameMode)
+                if trumpInHand:
+                    return trumpInHand
+                else:
+                    return playerHand
+            else:
+                if self.gameMode[0] == 2:
+                    cardsOfSuit = getCardsOfSuit(playerHand, card0.suit, ['U'])
+                else:
+                    cardsOfSuit = getCardsOfSuit(playerHand, card0.suit, ['U', 'O'])
+                if cardsOfSuit:
+                    return cardsOfSuit
+                else:
+                    return playerHand
+
     def getNextValidAction(self):
         currentPlayerIndex = (len(self.history) + self.leadingPlayer) % 4
-        playerHand = self.gameDict['playersHands'][currentPlayerIndex]
+        playerHand = deepcopy(self.gameDict['playersHands'][currentPlayerIndex])
         validCards = self.getValidActionsForHand(playerHand)
         return validCards, currentPlayerIndex, playerHand, self.history
 
     def nextAction(self):
         currentPlayerIndex = (len(self.history) + self.leadingPlayer) % 4
-        playerHand = self.gameDict['playersHands'][currentPlayerIndex]
+        playerHand = deepcopy(self.gameDict['playersHands'][currentPlayerIndex])
         validCards = self.getValidActionsForHand(playerHand)
+        t = None
 
         self.players[currentPlayerIndex].setHand(playerHand)
         playedCard = self.players[currentPlayerIndex].playCard(validCards, self.gameDict, self.history)
@@ -82,7 +178,7 @@ class Trick:
 
     # returns all suits from hand without trumps. If empty return hand
     def getSuitsInHand(self, suit, hand):
-        trumps = createTrumps(self.gameMode)
+        trumps = set(createTrumpsList(self.gameMode))
         cards = list([x for x in hand if x.suit == suit and x not in trumps])
         if not cards:
             return hand
@@ -90,9 +186,10 @@ class Trick:
             return cards
 
     # Adapted from Taschee Github https://github.com/Taschee/schafkopf/blob/master/schafkopf/trick_game.py
-    def getValidActionsForHand(self, playerHand):
+    def getValidActionsForHandOld(self, playerHand):
+        history = self.history
         hand = set(playerHand)
-        trumps = createTrumps(self.gameMode)
+        trumps = set(createTrumpsList(self.gameMode))
         # hack because Wenz (2,NONE) returns key error #TODO
         if self.gameMode[0] != 2:
             searchedSuit = REVERSEDSUITS[self.gameMode[1]]
@@ -138,13 +235,12 @@ class Trick:
         if self.gameMode[0] == 1 and Card(searchedSuit, 'A') in possibleActions:
             if not self.gameDict['ranAway'] and len(possibleActions) > 1:
                 possibleActions.remove(Card(searchedSuit, 'A'))
-        return possibleActions
+        return list(possibleActions)
 
     def copy(self):
         trick = Trick(self.gameDict, self.leadingPlayer)
         return trick
 
-    #OpenAI stuff
-    def addCard(self,card):
+    # OpenAI stuff
+    def addCard(self, card):
         self.history.append(card)
-
