4bf1a8e628d89fdd0c2d9d02c87d665238d789c8
TimiH <hestim@gmail.com>
2021-06-30 17:51:11 +0200

    innit

--- PlayerModels/PPO/openEnv.py
+++ PlayerModels/PPO/openEnv.py
@@ -1,2 +1,38 @@
 from Game import Game
+import gym
+from gym import spaces
+from PlayerModels.RandomPlayer import RandomPlayer
 
+
+class SchafkopfEnv(gym.Env):
+    """Schafkopf environment for OpenAI gym"""
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self, players, lead, seed=None):
+        super.(SchafkopfEnv, self).__innit__()
+        self.players = players
+        self.game = Game(players, 0)
+        self.game.setupGame()
+        self.game.playBidding()
+        if self.game.gameMode == (0, 0)
+            self.reset()
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        self.game = Game(self.players, 0)
+        self.game.setupGame()
+        self.game.playBidding()
+        self.game.continueTillNextAction()
+        while self.game.gameMode == (0, 0):
+            self.game = Game(self.players, 0)
+            self.game.setupGame()
+            self.game.playBidding()
+            self.game.continueTillNextAction()
+
+        return self.game.currentTrick.getNextValidAction()
+
+    # starts new game
+    def render(self, mode='human'):
+        pass
