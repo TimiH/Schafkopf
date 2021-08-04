a092936a49336574d2cb9e351072ab5a73a31e1e
TimiH <hestim@gmail.com>
2021-08-04 12:15:29 +0200

    trainhard for ssh

--- PlayerModels/PPO/trainHard.py
+++ PlayerModels/PPO/trainHard.py
@@ -1,13 +1,13 @@
 from PlayerModels.PPO.trainSettings import TrainSettings
 from PlayerModels.PPO.trainRL import main
 
-tSettings = TrainSettings('a001', colab=False)
+tSettings = TrainSettings('new', colab=False)
 
 tSettings.episodes = 200
 tSettings.eval_rounds = 1000
 tSettings.eval_interval = 5
 
-tSettings.update_games = 100000
+tSettings.update_games = 30000
 tSettings.batch_size = tSettings.update_games * 8 * 4
 tSettings.mini_batch_size = tSettings.batch_size
 
