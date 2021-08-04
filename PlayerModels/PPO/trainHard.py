88e862c3258cc3adf627526bc64e5571c65467e2
TimiH <hestim@gmail.com>
2021-08-04 12:23:33 +0200

    trainhard for ssh

--- PlayerModels/PPO/trainHard.py
+++ PlayerModels/PPO/trainHard.py
@@ -1,6 +1,8 @@
 import os
+import sys
 
-print(os.getcwd())
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print(sys.path)
 from PlayerModels.PPO.trainSettings import TrainSettings
 from PlayerModels.PPO.trainRL import main
 
