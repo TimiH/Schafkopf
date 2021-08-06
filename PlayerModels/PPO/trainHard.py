244905d3e35e36e2615270e05ac2b9f827fade31
TimiH <hestim@gmail.com>
2021-08-06 15:01:06 +0200

    clean up

--- PlayerModels/PPO/trainHard.py
+++ PlayerModels/PPO/trainHard.py
@@ -4,17 +4,17 @@ import sys
 from PlayerModels.PPO.trainSettings import TrainSettings
 from PlayerModels.PPO.trainRL import main
 
-tSettings = TrainSettings('125', colab=False)
+tSettings = TrainSettings('test', colab=False)
 
 tSettings.episodes = 200
-tSettings.eval_rounds = 1000
-tSettings.eval_interval = 10000  # error 10
-tSettings.lr_stepsize = 10000000
-tSettings.update_games = 200
-tSettings.batch_size = tSettings.update_games * 8 * 4
+tSettings.eval_rounds = 5000
+tSettings.eval_interval = 5
+tSettings.lr_stepsize = 30000000
+tSettings.update_games = 8000
+tSettings.batch_size = int(tSettings.update_games * 8 * 4 * 0.8)
 tSettings.mini_batch_size = tSettings.batch_size
 
-tSettings.K_epochs = 12
+tSettings.K_epochs = 16
 
 # tSettings.save()
 main(tSettings)
--- test.py
+++ test.py
@@ -1,16 +1,20 @@
 from PlayerModels.RandomPlayer import RandomPlayer
 from PlayerModels.HeuristicPlayer import HeuristicPlayer
 # from PlayerModels.RandomSample import RandomSample
+from PlayerModels.Player import Player
 from Tournament import playFairTournament, playRandomTournament
 from PlayerModels.PPO.LinearPolicy import LinearModel
+from PlayerModels.PPO.SoloPolicy import SoloModel
 from PlayerModels.ModelPlayer import ModelPlayer
+from PlayerModels.SeperatedModelPlayer import SeperatedModelPlayer
 import time
+import torch
 
 # p1 = RandomPlayer("RandomPlayer")
-# 2v2
+# # 2
 # p1 = HeuristicPlayer("HeuristicPlayer0")
+# p3= RandomPlayer("RandomPlayer0")
 # p2 = HeuristicPlayer("HeuristicPlayer1")
-# p3 = RandomPlayer("RandomPlayer0")
 # p4 = RandomPlayer("RandomPlayer1")
 # 3v1
 # p1 = HeuristicPlayer("HeuristicPlayer0")
@@ -23,21 +27,30 @@ import time
 # p3 = HeuristicPlayer("HeuristicPlayer2")
 # p4 = RandomPlayer("RandomPlayer0")
 
-p1 = ModelPlayer('Mode0', LinearModel(), eval=True)
-p2 = ModelPlayer('Model', LinearModel(), eval=True)
-p3 = ModelPlayer('Mode2', LinearModel(), eval=True)
-p4 = ModelPlayer('Mode3', LinearModel(), eval=True)
+# p1 = ModelPlayer('Mode0', LinearModel(), eval=True)
+# p2 = ModelPlayer('Model', LinearModel(), eval=True)
+# p3 = ModelPlayer('Mode2', LinearModel(), eval=True)
+# p4 = ModelPlayer('Mode3', LinearModel(), eval=True)
 
 # #4H
-# p1 = HeuristicPlayer("HeuristicPlayer0")
-# p2 = HeuristicPlayer("HeuristicPlayer1")
-# p3 = HeuristicPlayer("HeuristicPlayer2")
-# p4 = HeuristicPlayer("HeuristicPlayer3")
+# p1 = HeuristicPlayer("HeuristicPlayer1")
+# p2 = HeuristicPlayer("HeuristicPlayer2")
+# p3 = HeuristicPlayer("HeuristicPlayer3")
+# p4 = HeuristicPlayer("HeuristicPlayer4")
 
+p1 = RandomPlayer("RandomPlayer0")
+p2 = RandomPlayer("RandomPlayer1")
+p3 = RandomPlayer("RandomPlayer2")
+p4 = RandomPlayer("RandomPlayer3")
 
+# soloModel = SoloModel()
+# soloModel.load_state_dict(torch.load('PlayerModels/PPO/checkpoints/runSample/873.pt',map_location=torch.device('cpu')))
+
+# p1 = SeperatedModelPlayer('Solo0',policySolo=soloModel,eval=True)
+# p3 = SeperatedModelPlayer('Solo3',policySolo=soloModel,eval=True)
 players = [p1, p2, p3, p4]
 start = time.time()
-stats = playFairTournament(players, 100, laufendeBool=False)
-# stats = playRandomTournament(players, 1000,laufendeBool=False)
+# stats = playFairTournament(players, 1000,mode=3,verbose=True, laufendeBool=False)
+stats = playRandomTournament(players, 20, mode=3, laufendeBool=False)
 end = time.time()
 print(("Time:", end - start))
