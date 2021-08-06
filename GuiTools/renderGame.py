e2ab7ab5f3189bf947a0edf7b7bdce8347a87196
TimiH <hestim@gmail.com>
2021-08-06 12:48:16 +0200

    done

--- GuiTools/renderGame.py
+++ GuiTools/renderGame.py
@@ -24,7 +24,7 @@ def renderGameState(gameState, trickHistory, validCards=None, position=None):
 
     # Game state handling
     lead = gameState['leadingPlayer']
-    trickHistory = rotateListBackwards(trickHistory, lead)  # breaks if len<4? hack fill up with none
+    # trickHistory = rotateListBackwards(trickHistory, lead)  # breaks if len<4? hack fill up with none
     gameMode = gameState['gameMode']
 
     # hands: ORDER 0123,SWNE
@@ -48,9 +48,11 @@ def renderGameState(gameState, trickHistory, validCards=None, position=None):
     # greyscale non valid cards
     validIndex = []
     if validCards:
-        currentPos = len(trickHistory)
+        currentPos = (len(trickHistory) + gameState['leadingPlayer']) % 4
+        # currentPos = len(trickHistory)
         for card in validCards:
             validIndex.append(handDict[currentPos].index(card))
+    currentPos = (len(trickHistory) + lead) % 4
 
     # handImages
     handS = getHandArray(handS)
@@ -67,13 +69,13 @@ def renderGameState(gameState, trickHistory, validCards=None, position=None):
         img.paste(card, pos)
     for key, card in enumerate(handN):
         card.thumbnail(SIZECARD)
-        if currentPos == 1 and key not in validIndex:
+        if currentPos == 2 and key not in validIndex:
             card = card.convert('LA')
         pos = (posN[0] + key * int(SIZECARD[0] / 2), posN[1])
         img.paste(card, pos)
     for key, card in enumerate(handW):
         card.thumbnail(SIZECARD)
-        if currentPos == 2 and key not in validIndex:
+        if currentPos == 1 and key not in validIndex:
             card = card.convert('LA')
         # card = card.transpose(Image.ROTATE_90)
         pos = (posW[0], posW[1] + key * int(SIZECARD[0] / 2))
@@ -90,10 +92,10 @@ def renderGameState(gameState, trickHistory, validCards=None, position=None):
     # cardPositions
     cardN = (int(WIDTH / 2) - int(SIZECARD[0] / 2), int(HEIGHT / 2) - int(SIZECARD[1]) - TRICKPADDING)
     cardS = (int(WIDTH / 2) - int(SIZECARD[0] / 2), int(HEIGHT / 2) + +TRICKPADDING)
-
     cardW = (int(WIDTH / 2) - int(SIZECARD[1]) - TRICKPADDING, int(HEIGHT / 2) - int(SIZECARD[1] / 2))
     cardE = (int(WIDTH / 2) + int(SIZECARD[1]) + TRICKPADDING, int(HEIGHT / 2) - int(SIZECARD[1] / 2))
-    cardPos = [cardS, cardW, cardN, cardW]
+
+    cardPos = rotateListBackwards([cardS, cardW, cardN, cardW], gameState['leadingPlayer'])
     for key, card in enumerate(trickHistory):
         image = getCardImg(card)
         image.thumbnail(SIZECARD)
@@ -108,19 +110,38 @@ def renderGameState(gameState, trickHistory, validCards=None, position=None):
     # playerNames
     NAMESPADDING = 20
     tPosS = (int(WIDTH / 2) - fontSize / 2, HEIGHT - SIZECARD[1] - NAMESPADDING - fontSize - 10)
-    tPosW = (WIDTH - SIZECARD[0] - PADDING - NAMESPADDING - fontSize - 10, int(HEIGHT / 2))
+    tPosE = (WIDTH - SIZECARD[0] - PADDING - fontSize - 10, int(HEIGHT / 2))
     tPosN = (int(WIDTH / 2) - fontSize / 2, SIZECARD[1] + NAMESPADDING)
-    tPosE = (SIZECARD[0] + PADDING, int(HEIGHT / 2))
+    tPosW = (SIZECARD[0] + PADDING, int(HEIGHT / 2))
     draw.text(tPosS, 'S', font=font)
     draw.text(tPosW, 'W', font=font)
     draw.text(tPosN, 'N', font=font)
     draw.text(tPosE, 'E', font=font)
     # Rectangle
     rectPos0 = (WIDTH / 2 + SIZEHAND[0] / 2, PADDING)
-    rectPos1 = (WIDTH - PADDING, SIZECARD[1] + PADDING)
-    draw.rectangle((rectPos0, rectPos1), fill='black')
+    rectPos1 = (WIDTH - PADDING + 20, SIZECARD[1] + PADDING + 10)
+    draw.rectangle((rectPos0, rectPos1), outline='black')
     # GameMode
+    dictMode = {1: 'TEAM', 2: 'WENZ', 3: 'SOLO'}
+    dictColor = {None: 'NONE', 0: 'EICHEL', 1: 'GRAS', 2: 'HERZ', 3: 'SCHELLEN'}
+    gameText = dictMode[gameMode[0]] + ', ' + dictColor[gameMode[1]]
     # Scores
+    scores = gameState['scores']
+    scoresText = '\nS:' + str(scores[0]) + '\nW:' + str(scores[1]) + '\nN:' + str(scores[2]) + '\nE:' + str(scores[3])
     # Teams
+    offensivePlayers = gameState['offensivePlayers']
+    teamArr = ['S', 'W', 'N', 'E']
+    offensiveTeam = [teamArr[x] for x in offensivePlayers]
+    oppTeam = [teamArr[x] for x in range(4) if teamArr[x] not in offensiveTeam]
+    teamText = '\nOFFENSIVE: \t' + str(offensiveTeam) + '\nOpposition:\t' + str(oppTeam)
+    # searched and run away
+    searchText = '\nSearched = ' + str(gameState['searched'])
+    ranAwayText = '\nRan Away = ' + str(gameState['ranAway'])
+
+    # Drawing gameState
+    fontSize = 15
+    font = ImageFont.truetype('/home/tim/Work/Schafkopf/GuiTools/font/Russo Sans Bold.otf', fontSize)
+    draw.text((rectPos0[0] + 3, rectPos0[1] + 3), gameText + scoresText + teamText + searchText + ranAwayText,
+              font=font)
 
     return img
