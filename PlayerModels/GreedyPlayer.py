c528793ab63c56d4ebb484f7c965730f8d906d8c
TimiH <hestim@gmail.com>
2021-08-06 18:22:48 +0200

    greedy Introduction

---
+++ PlayerModels/GreedyPlayer.py
@@ -0,0 +1,57 @@
+from PlayerModels.Player import Player
+from PlayerModels.staticBidding import choseSoloGame, choseWenzGameRevised, choseTeamGame, choseWenzGameSimple
+
+from PlayerModels.staticBidding import getCardsOfRank, cardInHand, getCardOfSuitRank, \
+    trumpsInHandByGamemode, getCardsOfSuit
+from helper import createTrumpsList, byRank, getTrickWinnerIndex, sumTrickHistory, ringTest, sortTrump, getParnterPos, \
+    getValidWinners
+
+
+class GreedyPlayer(Player):
+    def makeBid(self, validBids):
+        teamGameChoice = choseTeamGame(validBids, self.hand)
+        wenzGameChoice = choseWenzGameSimple(self.hand)
+        soloGameChoice = choseSoloGame(validBids, self.hand)
+        bids = []
+
+        max = (0, 0)
+        if teamGameChoice[0] > max[0]:
+            max = teamGameChoice
+        if wenzGameChoice[0] > max[0]:
+            max = wenzGameChoice
+        if soloGameChoice[0] > max[0]:
+            max = soloGameChoice
+        # print("PLAYERCHOICES",max,teamGameChoice,wenzGameChoice,soloGameChoice)
+        # if max != (0, 0):
+        #     print((max, self.hand))
+        return max
+
+    def playCard(self, validCards, gameDict, trickHistory):
+        if len(validCards) == 1:
+            return validCards[0]
+
+        gameMode = gameDict['gameMode']
+        trumps = createTrumpsList(gameMode)
+        # Lead play highest Card
+        if len(trickHistory) == 0:
+            # play highest Card
+            trumpsInHand = trumpsInHandByGamemode(validCards, gameMode)
+            if trumpsInHand:
+                orderedTrump = sortTrump(trumpsInHand)
+                card = orderedTrump[0]
+            else:
+                card = min(validCards, key=byRank)
+        else:
+            # win or min
+            winningCards = getValidWinners(trickHistory, validCards, gameMode)
+            if winningCards:
+                card = max(winningCards, key=lambda x: x[1])[0]
+            else:
+                validNoTrump = list(set(validCards) - set(trumps))
+                if validNoTrump:
+                    card = min(validNoTrump, key=lambda x: x.value)
+                else:
+                    card = min(validCards, key=lambda x: x.value)
+        if not card:
+            raise Exception
+        return card
